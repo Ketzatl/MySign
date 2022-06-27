6b394b27b1cd6510929b7d57f5f76f7ed114221a
Antoine GOUPIL <antoine.goupil1998@gmail.com>
2022-06-27 14:24:59 +0200

    Début stucture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpReqInterceptor } from './core/interceptors/http-req.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpReqInterceptor, multi: true },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
