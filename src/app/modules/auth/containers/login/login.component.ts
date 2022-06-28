9f437a1cd737ddc27c61e0d2af64436f87a9ab96
Antoine GOUPIL <antoine.goupil1998@gmail.com>
2022-06-28 16:59:13 +0200

    Unsuscribe ajouté

--- src/app/modules/auth/containers/login/login.component.ts
+++ src/app/modules/auth/containers/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ToastProcessingService } from 'src/app/core/services/toast-processing.service';
 import { LoginDto } from '../../dto/login.dto';
 import { AuthService } from '../../services/auth.service';
@@ -10,8 +11,9 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
+  listSubScriptions: Subscription[] = [];
 
   constructor(
     private formBuider: FormBuilder,
@@ -33,23 +35,29 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      this.authService.login(this.loginForm.value as LoginDto).subscribe({
-        next: (value) => {
-          console.log(value);
-          this.toastProcessingService.success(
-            'Bienvenue !',
-            'Bienvenue sur MySign'
-          );
-          this.router.navigate(['']);
-        },
-        error: (err) =>
-          this.toastProcessingService.error(
-            'Erreur',
-            'Identifiant ou mot de passe incorrect !'
-          ),
-      });
+      this.listSubScriptions.push(
+        this.authService.login(this.loginForm.value as LoginDto).subscribe({
+          next: (value) => {
+            console.log(value);
+            this.toastProcessingService.success(
+              'Bienvenue !',
+              'Bienvenue sur MySign'
+            );
+            this.router.navigate(['']);
+          },
+          error: (err) =>
+            this.toastProcessingService.error(
+              'Erreur',
+              'Identifiant ou mot de passe incorrect !'
+            ),
+        })
+      );
     } else {
       this.toastProcessingService.error('Erreur', 'Champs incorrect');
     }
+  }
+
+  ngOnDestroy(): void {
+    this.listSubScriptions.forEach((s) => s.unsubscribe());
   }
 }
